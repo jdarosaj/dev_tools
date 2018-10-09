ef8cd60a0021e999e5e6649ca9a2f63bd6aab6fe
Jair Rosa <jair.rosa@khomp.com>
2018-10-09 12:29:41 -0300

    exclude img file

--- scripts/compress_project.sh
+++ scripts/compress_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-rsync -av --progress /root/kmgmanager/* /tmp/kmgmanager/ --exclude '.git*'
+rsync -av --progress /root/kmgmanager/* /tmp/kmgmanager/ --exclude '.git*' --exclude '*img'
 cd /tmp/
 tar -zcvf /root/kmgmanager.tar.gz ./kmgmanager/
 echo "The tar.gz was created on /root/kmgmanager.tar.gz!"
